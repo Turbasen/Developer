da8b3375a9bd3adc4f8f1822c44291a8ed88d7dd
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-04-22 20:48:57 +0200

    feat(app): add user profile app
    Signed-off-by: Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>

---
+++ apps/profile/controller.js
@@ -0,0 +1,168 @@
+/* eslint no-unused-vars: 0 */
+'use strict';
+
+const router = require('express').Router;
+
+const app = router();
+const ApiUser = require('../app/model').ApiUser;
+
+const NTB_TOS_VERSION = process.env.NTB_TOS_VERSION || 1;
+
+// get api user for authenticated user
+app.use('/', (req, res, next) => {
+  const query = {
+    'owner.userId': req.session.auth.userId,
+  };
+
+  ApiUser.findOne(query, (err, api) => {
+    if (err) { return next(err); }
+
+    req.api = api || new ApiUser({
+      provider: req.session.auth.fullName.split(' ').reverse()[0].toUpperCase(),
+      contact: {
+        name: req.session.auth.fullName,
+        email: req.session.auth.email,
+      },
+    });
+
+    return next();
+  });
+});
+
+app.get('/', (req, res, next) => {
+  const user = req.session.auth;
+  const error = req.session.message;
+
+  delete req.session.message;
+  res.render('profile/index.html', { req, user, api: req.api, error });
+  return;
+});
+
+app.post('/link', (req, res, next) => {
+  // Existing users can not link their API-key
+  if (!req.api.isNew) {
+    req.session.message = {
+      message: 'Din profil kan ikke linkes til en API-nøkkel!',
+      field: 'api_key',
+    };
+
+    res.set('x-app-status', 'failure');
+    res.set('x-app-message', 'link_forbidden');
+    res.redirect(403, '/profile');
+
+    return;
+  }
+
+  // Check if api_key is provided
+  if (!req.body.api_key) {
+    req.session.message = {
+      message: 'API-nøkkel kan ikke være tom!',
+      field: 'api_key',
+    };
+
+    res.set('x-app-status', 'failure');
+    res.set('x-app-message', 'empty_key');
+    res.redirect(303, '/profile');
+
+    return;
+  }
+
+  const query = {
+    'apps.key.prod': req.body.api_key,
+  };
+
+  ApiUser.findOne(query).exists('owner', false).exec((findErr, doc) => {
+    if (findErr) { next(findErr); return; }
+
+    if (!doc) {
+      req.session.message = {
+        message: 'API-nøkkelen eksisterer ikke eller er allerede linket.',
+        field: 'api_key',
+      };
+
+      res.set('x-app-status', 'failure');
+      res.set('x-app-message', 'key_not_found');
+      res.redirect(303, '/profile');
+
+      return;
+    }
+
+    req.api = doc;
+
+    req.api.owner = req.session.auth;
+    req.api.save({ validateBeforeSave: false }, (saveErr) => {
+      if (saveErr) { next(saveErr); return; }
+
+      req.session.message = {
+        class: 'positive',
+        title: 'Profil linket',
+        message: 'Din profil ble suksessfullt linket!',
+      };
+
+      res.set('x-app-status', 'success');
+      res.redirect(303, '/profile');
+
+      return;
+    });
+
+    return;
+  });
+
+  return;
+});
+
+app.post('/', (req, res, next) => {
+  const user = req.session.auth;
+
+  // New User
+  if (req.api.isNew) {
+    req.api.provider = req.body.provider;
+    req.api.owner = req.session.auth;
+  }
+
+  req.api.contact.name = req.body.name;
+  req.api.contact.email = req.body.email;
+  req.api.contact.phone = req.body.phone;
+
+  req.api.terms = parseInt(req.body.terms, 10) || 0;
+  req.api.notify = req.body.notify === '1';
+
+  const validation = req.api.validateSync();
+
+  if (validation) {
+    const key = Object.keys(validation.errors)[0];
+    req.session.message = validation.errors[key];
+
+    res.set('x-app-status', 'failure');
+    res.set('x-app-message', 'validation_error');
+    res.redirect(303, '/profile');
+
+    return;
+  }
+
+  req.api.save((saveErr) => {
+    if (saveErr && saveErr.code !== 11000) { next(saveErr); return; }
+
+    if (saveErr && saveErr.code === 11000) {
+      req.session.message = saveErr;
+
+      res.set('x-app-status', 'failure');
+      res.set('x-app-message', 'validation_error');
+      res.redirect(303, '/profile');
+    } else {
+      req.session.message = {
+        class: 'positive',
+        title: 'Profil oppdatert',
+        message: 'Din profil ble suksessfullt oppdatert',
+      };
+
+      res.set('x-app-status', 'success');
+      res.redirect(303, '/profile');
+    }
+
+    return;
+  });
+  return;
+});
+
+module.exports = app;
---
+++ test/acceptance/profile.js
@@ -0,0 +1,286 @@
+'use strict';
+
+const assert = require('assert');
+const supertest = require('supertest');
+const req = supertest(require('../../').app);
+
+const ApiUser = require('../../apps/app/model').ApiUser;
+
+describe('GET /profile', () => {
+  const url = '/profile';
+
+  it('redirects to login for unauthenticated user', done => {
+    req.get(url)
+     .expect(302)
+     .expect('Location', '/login', done);
+  });
+
+  it('returns profile form for new user', done => {
+    req.get(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .expect(200, done);
+  });
+
+  it('returns profile form for existin user', done => {
+    req.get(url)
+      .set('Cookie', process.env.USER_WITH_APPS_COOKIE)
+      .expect(200, done);
+  });
+});
+
+describe('POST /profile/link', () => {
+  const url = '/profile/link';
+
+  it('returns error for linking existing profile', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_WITH_APPS_COOKIE)
+      .type('form')
+      .send({ api_key: 'fookey' })
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'link_forbidden')
+      .expect(403, done);
+  });
+
+  it('returns error for linking empty api key', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'empty_key')
+      .expect(303, done);
+  });
+
+  it('returns error for linking invalid api key', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({ api_key: 'abc123' })
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'key_not_found')
+      .expect(303, done);
+  });
+
+  it('links new profile to existing api key', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({ api_key: 'fookey' })
+      .expect(303, done);
+  });
+});
+
+describe('POST /profile', () => {
+  const url = '/profile';
+
+  it('returns error for empty provider name', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({
+        name: 'Foo Bar',
+        email: 'foo@bar.com',
+        phone: '99988777',
+        terms: '1',
+      })
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'validation_error')
+      .expect(303, done);
+  });
+
+  it('returns error for invalid provider name', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({
+        provider: 'Foo Bar',
+        name: 'Foo Bar',
+        email: 'foo@bar.com',
+        phone: '99988777',
+        terms: '1',
+      })
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'validation_error')
+      .expect(303, done);
+  });
+
+  it('returns error for duplicate provider name', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({
+        provider: 'FOO',
+        name: 'Foo Bar',
+        email: 'foo@bar.com',
+        phone: '99988777',
+        terms: '1',
+      })
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'validation_error')
+      .expect(303, done);
+  });
+
+  it('returns error for empty name', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({
+        provider: 'FOOBAR',
+        email: 'foo@bar.com',
+        phone: '99988777',
+        terms: '1',
+      })
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'validation_error')
+      .expect(303, done);
+  });
+
+  it('returns error for empty email', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({
+        provider: 'FOOBAR',
+        name: 'Foo Bar',
+        phone: '99988777',
+        terms: '1',
+      })
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'validation_error')
+      .expect(303, done);
+  });
+
+  it('returns error for empty phone', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({
+        provider: 'FOOBAR',
+        name: 'Foo Bar',
+        email: 'foo@bar.com',
+        terms: '1',
+      })
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'validation_error')
+      .expect(303, done);
+  });
+
+  it('returns error for empty tos', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({
+        provider: 'FOOBAR',
+        name: 'Foo Bar',
+        email: 'foo@bar.com',
+        phone: '99988777',
+      })
+      .expect('x-app-status', 'failure')
+      .expect('x-app-message', 'validation_error')
+      .expect(303, done);
+  });
+
+  it('creates a new profile to the database', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_NO_PROFILE_COOKIE)
+      .type('form')
+      .send({
+        provider: 'FOOBAR',
+        name: 'Foo Bar',
+        email: 'foo@bar.com',
+        phone: '99988777',
+        terms: '1',
+      })
+      .expect('x-app-status', 'success')
+      .expect(303)
+      .end(err => {
+        if (err) { return done(err); }
+
+        const query = { 'owner.userName': process.env.USER_NO_PROFILE_OWNER };
+        const proj = '';
+        const opts = {};
+
+        return ApiUser.findOne(query, proj, opts, (mongoErr, api) => {
+          if (mongoErr) { return done(mongoErr); }
+
+          assert.equal(api.provider, 'FOOBAR');
+          assert.equal(api.contact.name, 'Foo Bar');
+          assert.equal(api.contact.email, 'foo@bar.com');
+          assert.equal(api.contact.phone, '99988777');
+          assert.equal(api.terms, 1);
+          assert.equal(api.notify, false);
+
+          return done();
+        });
+      });
+  });
+
+  it('updates invalid profile in the database', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_INVALID_PROFILE_COOKIE)
+      .type('form')
+      .send({
+        provider: 'FOOBAR',
+        name: 'Foo Bar',
+        email: 'foo@bar.com',
+        phone: '99988777',
+        terms: '1',
+      })
+      .expect('x-app-status', 'success')
+      .expect(303)
+      .end(err => {
+        if (err) { return done(err); }
+
+        const query = { 'owner.userName': process.env.USER_INVALID_PROFILE_OWNER };
+        const proj = '';
+        const opts = {};
+
+        return ApiUser.findOne(query, proj, opts, (mongoErr, api) => {
+          if (mongoErr) { return done(mongoErr); }
+
+          assert.equal(api.provider, 'BAR');
+          assert.equal(api.contact.name, 'Foo Bar');
+          assert.equal(api.contact.email, 'foo@bar.com');
+          assert.equal(api.contact.phone, '99988777');
+          assert.equal(api.terms, 1);
+          assert.equal(api.notify, false);
+
+          return done();
+        });
+      });
+  });
+
+  it('updates existing profile in the database', (done) => {
+    req.post(url)
+      .set('Cookie', process.env.USER_WITH_APPS_COOKIE)
+      .type('form')
+      .send({
+        provider: 'FOOBAR',
+        name: 'Foo Bar',
+        email: 'foo@bar.com',
+        phone: '99988777',
+        terms: '1',
+      })
+      .expect('x-app-status', 'success')
+      .expect(303)
+      .end(err => {
+        if (err) { return done(err); }
+
+        const query = { 'owner.userName': process.env.USER_WITH_APPS_OWNER };
+        const proj = '';
+        const opts = {};
+
+        return ApiUser.findOne(query, proj, opts, (mongoErr, api) => {
+          if (mongoErr) { return done(mongoErr); }
+
+          assert.equal(api.provider, 'BAZ');
+          assert.equal(api.contact.name, 'Foo Bar');
+          assert.equal(api.contact.email, 'foo@bar.com');
+          assert.equal(api.contact.phone, '99988777');
+          assert.equal(api.terms, 1);
+          assert.equal(api.notify, false);
+
+          return done();
+        });
+      });
+  });
+});
