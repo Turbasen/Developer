13e65a75a44656f7d9896a9d55a5eab6d92e687c
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-04-22 20:44:47 +0200

    feat(app): add login and authentication app
    Signed-off-by: Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>

---
+++ apps/auth/controller.js
@@ -0,0 +1,107 @@
+/* eslint no-console: 0*/
+'use strict';
+
+const router = require('express').Router;
+const app = router();
+const ApiUser = require('../app/model').ApiUser;
+
+const github = require('../../lib/github');
+const OAuth2 = require('oauth').OAuth2;
+const oauth2 = new OAuth2(
+  process.env.GH_OAUTH_CLIENT,
+  process.env.GH_OAUTH_SECRET,
+  'https://github.com/',
+  'login/oauth/authorize',
+  'login/oauth/access_token',
+  null
+);
+
+const qs = require('querystring');
+const admins = new Set(process.env.APP_ADMINS.split(','));
+
+app.get('/login', (req, res) => {
+  const error = req.query.error;
+  const errorDescription = req.query.error_description;
+
+  res.render('login.html', { req, error, errorDescription });
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+});
+
+app.get('/login/github', (req, res) => {
+  const originalUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+
+  const authURL = oauth2.getAuthorizeUrl({
+    redirect_uri: `${originalUrl}/callback`,
+    scope: 'repo,user',
+    state: process.env.GH_OAUTH_RANDOM,
+  });
+
+  res.redirect(authURL);
+});
+
+app.get('/login/github/callback', (req, res, next) => {
+  const originalUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+
+  // @TODO validate req.query.state ?
+  // @TODO req.query.error + req.query.error_description
+
+  if (req.query.error) {
+    console.log(req.query);
+    return res.redirect(`/login?${qs.stringify({
+      error: req.query.error,
+      error_description: req.query.error_description,
+    })}`);
+  }
+
+  return oauth2.getOAuthAccessToken(
+    req.query.code,
+    { redirect_uri: originalUrl },
+    (err, accessToken, refreshToken, results) => {
+      if (err) { return next(err); }
+      if (results.error) { return next(new Error(results.error_description)); }
+
+      const auth = { accessToken, refreshToken };
+
+      return github.client(auth).user.get({}, (userErr, userData) => {
+        if (userErr) { return next(userErr); }
+
+        req.session.auth = {
+          userId: userData.id,
+          userName: userData.login,
+          email: userData.email,
+          fullName: userData.name,
+          accessToken,
+          refreshToken,
+          avatarUrl: userData.avatar_url,
+          isAdmin: admins.has(userData.login),
+        };
+
+        if (!req.session.auth.isAdmin) {
+          return res.redirect('/');
+        }
+
+        return ApiUser.aggregate([
+          { $unwind: '$apps' },
+          { $group: {
+            _id: 'TOTOAL',
+            active: { $sum: { $cond: ['$apps.active', 1, 0] } },
+            inactive: { $sum: { $cond: ['$apps.active', 0, 1] } },
+            pending: { $sum: { $cond: ['$apps.approved', 0, 1] } },
+            request: { $sum: { $cond: ['$apps.limit.rodRequest', 1, 0] } },
+          } },
+        ], (aggregateErr, result) => {
+          if (aggregateErr) { return next(aggregateErr); }
+
+          req.session.stats = result[0];
+          return res.redirect('/');
+        });
+      });
+    });
+});
+
+module.exports = app;
---
+++ apps/auth/middleware.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const github = require('../../lib/github');
+// const User = require('./model').User;
+
+module.exports = function authMiddleware(req, res, next) {
+  if (!req.session) {
+    req.session = {};
+  }
+
+  if (!req.session.auth
+      && !/^\/favicon.ico/.test(req.originalUrl)
+      && !/^\/login/.test(req.originalUrl)
+      && !/^\/page\//.test(req.originalUrl)
+      && !/^\/app(\/[^\/]+){2}\/(webhook|[0-9a-f]{40})\/?$/.test(req.originalUrl)
+  ) {
+    return res.redirect('/login');
+  }
+
+  if (req.session.auth) {
+    if (!req.session.auth.accessToken) {
+      throw new Error(`Missing accessToken for user ${req.session.auth.userName}`);
+    }
+
+    req.github = github.client(req.session.auth);
+  }
+
+  return next();
+};
