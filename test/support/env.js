928a7427e2225eff0fbbbd8833a3f0af33a4bb69
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-04-22 20:39:29 +0200

    feat(package): add npm package configuration
    Signed-off-by: Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>

---
+++ test/support/env.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const sessions = require('../fixtures/sessions');
+const users = require('../fixtures/users');
+
+process.env.NODE_ENV = 'test';
+delete process.env.MONGO_URI;
+
+process.env.USER_NO_PROFILE_COOKIE = sessions[0].cookie;
+process.env.USER_NO_PROFILE_OWNER = users[0].userName;
+
+process.env.USER_INVALID_PROFILE_COOKIE = sessions[1].cookie;
+process.env.USER_INVALID_PROFILE_OWNER = users[1].userName;
+
+process.env.USER_WITH_APPS_COOKIE = sessions[2].cookie;
+process.env.USER_WITH_APPS_OWNER = users[2].userName;
+
+process.env.USER_NO_APPS_COOKIE = sessions[3].cookie;
+process.env.USER_NO_APPS_OWNER = users[3].userName;
