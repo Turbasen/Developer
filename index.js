45158222e4f9c6f82024885ac77f0bda3991cfb7
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-04-22 20:41:53 +0200

    feat(server): add core server runtime definition
    Signed-off-by: Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>

---
+++ index.js
@@ -0,0 +1,64 @@
+/* eslint no-console: 0, no-unused-vars: 0 */
+'use strict';
+
+const express = require('express');
+const statics = express.static;
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const db = require('./lib/db');
+
+const app = module.exports.app = express();
+app.set('x-powered-by', false);
+app.set('trust proxy', 1);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/static', statics(`${__dirname}/static`));
+app.use('/themes', statics(`${__dirname}/node_modules/semantic-ui-less/themes`));
+
+module.exports.nunjucks = nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  noCache: process.env.NODE_ENV !== 'production',
+});
+
+module.exports.nunjucks.addGlobal('foo', 'bar');
+
+app.get('/favicon.ico', (req, res) => {
+  res.set('Content-Type', 'image/x-icon');
+  res.end();
+});
+
+app.use(require('./apps/auth/session'));
+app.use(require('./apps/auth/middleware'));
+app.use('/', require('./apps/auth/controller'));
+app.use('/profile', require('./apps/profile/controller'));
+app.use('/app', require('./apps/app/controller'));
+
+// Redirect to /app
+app.get('/', (req, res) => {
+  res.redirect('/app');
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  if (!err.code) { err.code = 500; }
+
+  if (err.code >= 500) {
+    console.error(err);
+    console.error(err.stack);
+  }
+
+  res.status(err.code || 500);
+  res.send(err.message);
+});
+
+if (!module.parent) {
+  app.listen(8080);
+  db.connection.once('open', () => {
+    console.log('Server is accepting new connections on port 8080');
+  });
+}
+
+process.on('SIGINT', process.exit.bind(process, 1));
---
+++ test/acceptance/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const supertest = require('supertest');
+const req = supertest(require('../../').app);
+
+describe('GET /favicon.ico', () => {
+  const url = '/favicon.ico';
+
+  it('returns about page', (done) => {
+    req.get(url)
+      .expect('Content-Type', 'image/x-icon')
+      .expect(200, done);
+  });
+});
+
+describe('GET /', () => {
+  const url = '/';
+
+  it('redirects to login for unathenticated user', (done) => {
+    req.get(url)
+      .expect('Location', '/login')
+      .expect(302, done);
+  });
+
+  it('redirect to app for authenticated user', (done) => {
+    req.get(url)
+      .set('Cookie', process.env.USER_WITH_APPS_COOKIE)
+      .expect('Location', '/app')
+      .expect(302, done);
+  });
+});
