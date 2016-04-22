46536b95f7cf5c50b4c201f9fd856933e81ff5a3
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-04-22 22:00:14 +0200

    chore(test): separate dev and test environment variables
    Signed-off-by: Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>

--- test/support/env.js
+++ test/support/env.js
@@ -4,7 +4,11 @@ const sessions = require('../fixtures/sessions');
 const users = require('../fixtures/users');
 
 process.env.NODE_ENV = 'test';
+process.env.MONGO_DB = 'test';
 delete process.env.MONGO_URI;
+
+process.env.APP_ADMINS = 'Foo,Bar';
+process.env.APP_SECRET = 'foobar';
 
 process.env.USER_NO_PROFILE_COOKIE = sessions[0].cookie;
 process.env.USER_NO_PROFILE_OWNER = users[0].userName;
